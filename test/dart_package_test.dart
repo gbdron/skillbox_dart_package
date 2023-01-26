aee25630eeb59f59d30ace2dc44a2686d0f67a79
bdron-t480 <bdon@mail.ru>
2023-01-27 01:59:38 +0300

    refactored tests

--- test/dart_package_test.dart
+++ test/dart_package_test.dart
@@ -12,21 +12,8 @@ import 'package:test/test.dart';
 
 void main() {
   group('Tests for:', () {
-    final calc = DelimetersCalculator();
-    final bin = BinaryString();
-    final strNum = StringNumbers();
-    final strMap = StringMap();
-    final strSet = StringSet();
-    final point1 = Point.getInitialPoint();
-    final point2 = Point.getUnitVector();
-    final point3 = Point(2, -5, 7);
-    final point4 = Point(3, 4, 5);
-    final num number = 19;
-    final admin = AdminUser('admin@mail.ru');
-    final general = GeneralUser('general@mail.ru');
-    final userMgr = UserManager();
-
     test('1. DelimetersCalculator', () {
+      final calc = DelimetersCalculator();
       expect(calc.gcd(15, 20), 5);
       expect(calc.gcd(12, 30), 6);
       expect(calc.lcm(3, 4), 12);
@@ -34,20 +21,24 @@ void main() {
     });
 
     test('2. Binary String', () {
+      final bin = BinaryString();
       expect(bin.intToBinary(3435532), '0000000001101000110110000001100');
       expect(bin.binaryToInt('0000000001101000110110000001100'), 3435532);
     });
 
     test('3. String Numbers', () {
+      final strNum = StringNumbers();
       expect(strNum.strToNumbers('2 ad3sf 3 dsf 4 afds 5'), [2, 3, 4, 5]);
     });
 
     test('4. String Map', () {
+      final strMap = StringMap();
       expect(strMap.strToMap('aa  b  c aa b c aa b c d'),
           {'aa': 3, 'b': 3, 'c': 3, 'd': 1});
     });
 
     test('5. String Set', () {
+      final strSet = StringSet();
       expect(
           strSet.strToSet(
               ['one', 'cat', 'dog', 'one', 'two', 'three', 'zero', 'zero']),
@@ -55,6 +46,10 @@ void main() {
     });
 
     test('6. Point', () {
+      final point1 = Point.getInitialPoint();
+      final point2 = Point.getUnitVector();
+      final point3 = Point(2, -5, 7);
+      final point4 = Point(3, 4, 5);
       expect(point1.distanceTo(point2).toStringAsFixed(4),
           1.7321.toStringAsFixed(4));
       expect(point3.distanceTo(point4).toStringAsFixed(4),
@@ -62,11 +57,16 @@ void main() {
     });
 
     test('7. Nth Root', () {
+      final num number = 19;
       expect(
           number.getNthRoot(3).toStringAsFixed(4), 2.6694.toStringAsFixed(4));
     });
 
     test('8. Users', () {
+      final admin = AdminUser('admin@mail.ru');
+      final general = GeneralUser('general@mail.ru');
+      final userMgr = UserManager();
+
       expect(admin.getMailSystem(), 'mail.ru');
       userMgr.addUser(admin);
       userMgr.addUser(general);
